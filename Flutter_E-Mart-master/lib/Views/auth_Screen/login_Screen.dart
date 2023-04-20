a37def1b5c416c28a4ff2fdac74654cac84a8dd4
tops-om <rathodom.tops@gmail.com>
2023-04-20 15:20:19 +0530

    This

---
+++ Flutter_E-Mart-master/lib/Views/auth_Screen/login_Screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Login_Screen extends StatelessWidget {
+  const Login_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
