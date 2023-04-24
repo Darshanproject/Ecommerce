7260f9c062369648de87ff77cc10ccb7f2373d11
Darshanproject <gadadarshan6184@gmail.com>
2023-04-24 15:20:35 +0530

    Done

--- Flutter_E-Mart-master/lib/Views/Cart_Screen/Cart_Screen.dart
+++ Flutter_E-Mart-master/lib/Views/Cart_Screen/Cart_Screen.dart
@@ -1,3 +1,5 @@
+import 'package:emart_app/WidgetsCommon/bg_widget.dart';
+import 'package:emart_app/consts/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -7,8 +9,11 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-    );
+    return bgWidget(
+        child: Scaffold(
+      appBar: AppBar(
+        title: categories.text.white.fontFamily(bold).make(),
+      ),
+    ));
   }
 }
--- Flutter_E-Mart-master/lib/consts/lists.dart
+++ Flutter_E-Mart-master/lib/consts/lists.dart
@@ -3,3 +3,11 @@ import 'package:emart_app/consts/consts.dart';
 const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
 
 const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+
+const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+const featuredImages1 = [imgS1, imgS2, imgS3];
+const featuredImages2 = [imgS4, imgS5, imgS6];
+
+const featuredTitel1 = [womendress, girlsDress, girlswatches];
+const featuredTitel2 = [boysglasses, mobilephone, tshirts];
