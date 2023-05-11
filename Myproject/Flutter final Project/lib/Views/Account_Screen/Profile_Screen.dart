2c2a7717c76d452c41db198a3fe8787d25e1d6d7
Darshanproject <gadadarshan6184@gmail.com>
2023-05-11 16:18:31 +0530

    Done

--- Myproject/Flutter final Project/lib/Views/Account_Screen/Profile_Screen.dart
+++ Myproject/Flutter final Project/lib/Views/Account_Screen/Profile_Screen.dart
@@ -1,3 +1,5 @@
+import 'package:emart_app/WidgetsCommon/bg_widget.dart';
+import 'package:emart_app/consts/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -7,8 +9,32 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
+    return bgWidget(
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Column(children: [
+              Image.asset(
+                imgProfile2,
+                width: 100,
+                fit: BoxFit.cover,
+              ).box.roundedFull.clip(Clip.antiAlias).make(),
+              Expanded(
+                  child: Column(
+                children: [
+                  "Dummy user".text.fontFamily(semibold).white.make(),
+                  "gadadarshan926@gmail.com"
+                      .text
+                      .fontFamily(semibold)
+                      .white
+                      .make(),
+                ],
+              ))
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
